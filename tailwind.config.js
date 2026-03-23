22af1006cc7ccc758cec78069f4fc4ff2cce913c
oHajjo <ohajjo@greta-bretagne-sud.fr>
2026-03-23 13:58:16 +0100

    fix: remove templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+    "!./src/pages/Templates",
+    "!./src/pages/Templates/**",
+  ],
   theme: {
     extend: {
       colors: {
